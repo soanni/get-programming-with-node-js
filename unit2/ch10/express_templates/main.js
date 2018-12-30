00f3e5a7baa2b7c6dd11fb61667d87cbc1fcb3dc
Andrey Solodov <andrey.solodov@aurea.com>
2018-12-30 16:23:25 +0400

    [chapter 10] added dynamic vies, layouts, partials

--- unit2/ch10/express_templates/main.js
+++ unit2/ch10/express_templates/main.js
@@ -1,11 +1,14 @@
 const express = require('express'),
 	homeController = require('./controllers/homeController'),
+	layouts = require('express-ejs-layouts'),
 	app = express();
 
 app.set('view engine', 'ejs');
 app.set('port', process.env.PORT || 3000);
 
-app.get('/name', homeController.respondWithName);
+app.use(layouts);
+
+app.get('/name/:myName', homeController.respondWithName);
 
 app.listen(app.get('port'), () => {
 	console.log(`Server is running at http://localhost:${app.get('port')}`);
